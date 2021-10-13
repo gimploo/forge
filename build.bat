eadb42763fce35985880c442cdca75a2c93a582c
Gokul Krishnan C M <gokulkrishnan949@gmail.com>
2021-10-13 17:33:26 +0530

    A working build script for c projects for windows

--- build.bat
+++ build.bat
@@ -1,23 +1,47 @@
 @echo off
 
-REM Required dependencies
-set DEPENDENCIES=SDL2 GLEW
+REM =============================================================================================
+REM                            -- WINDOWS BUILD SCRIPT FOR C PROJECTS --
+REM =============================================================================================
+
+REM Include required dependencies
+set DEPENDENCY_LIST=SDL2 GLEW
 set SDL2_URL=https://www.libsdl.org/release/SDL2-devel-2.0.16-VC.zip
 set GLEW_URL=https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0-win32.zip
 
-
-REM Compiler of choice, MSVC
+REM Include compiler of choice (here its msvc)
 set CC=cl
 set CC_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+set CC_DEFAULT_FLAGS=/W4 /WX /wd4996 /wd5105 /FC /TC /Zi 
+set CC_DEFAULT_LIBS=opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
+REM Source and executalble path (default)
+set EXE_DEFAULT_PATH=.\bin
+set SRC_DEFAULT_PATH=.\src
+set DEPENDENCY_DEFAULT_PATH=.\external
+
+REM Source files and exe name
+set SRC_FILES=test.c
+set EXE=test.exe
+
+
+REM =============================================================================================
+REM                            -- WINDOWS BUILD SCRIPT FOR C PROJECTS --
+REM =============================================================================================
 
 
 :main
+    if "%1" == "deepclean" (
+        call :deepcleanup
+        exit /b 0
+    )
+
     if "%1" == "clean" (
         call :cleanup
         exit /b 0
     )
 
+    cls
     echo [*] Running build script for windows...
     
     echo [*] Checking %CC% compiler is installed ...
@@ -25,33 +49,105 @@ set CC_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Au
 
 
     echo [*] Checking if all dependenices are installed ...
-    if exist external (
+    if exist "%DEPENDENCY_DEFAULT_PATH%" (
         echo [!] External directory found!
     ) else (
         echo [!] External directory not found!
-        mkdir external
+        mkdir "%DEPENDENCY_DEFAULT_PATH%"
     )
 
-    echo [!] Checking dependenices ...
+    echo [*] Checking dependenices ...
     call :check_dependencies_are_installed 
+    echo [!] Dependencies all found!
 
+    if exist bin (
+        echo [!] Bin directory found!
+    ) else (
+        echo [!] Bin directory not found!
+        mkdir bin
+        echo [!] Bin directory made!
+    )
 
+    echo [*] Building project ...
+    call :build_project_with_msvc
+
+    if %errorlevel% == 0 (
+        echo [*] Running executable ...
+        call :run_executable
+
+        if %errorlevel% neq 0 (
+            echo [*] Running executable through the debugger ...
+            call :run_executable_with_debugger
+        )
+    )
+    
+
+    echo [!] Exited! 
     exit /b 0
 
 
-:cleanup
+REM ==================================================================================
+REM                         -- BUILD RECIPE --
+REM ==================================================================================
+REM (change whats in here to ) -
+REM                            |
+REM                            v
+:build_project_with_msvc
+    SETLOCAL
+
+    set INCLUDES_PATH=/I %DEPENDENCY_DEFAULT_PATH%\SDL2\include ^
+                    /I %DEPENDENCY_DEFAULT_PATH%\GLEW\include
+
+    set LIBS=%DEPENDENCY_DEFAULT_PATH%\SDL2\lib\x64\SDL2.lib ^
+                %DEPENDENCY_DEFAULT_PATH%\SDL2\lib\x64\SDL2main.lib ^
+                %DEPENDENCY_DEFAULT_PATH%\GLEW\lib\Release\x64\glew32.lib
+
+    cl %CC_DEFAULT_FLAGS% ^
+        %INCLUDES_PATH% ^
+        /Fe%EXE_DEFAULT_PATH%\%EXE% ^
+        %SRC_DEFAULT_PATH%\%SRC_FILES% ^
+        /link %CC_DEFAULT_LIBS% %LIBS% -SUBSYSTEM:windows
+
+
+    ENDLOCAL
+    exit /b %errorlevel%
+
+
+
+
+REM =======================================================================================
+REM                             -- HELPER FUNCTIONS --
+REM =======================================================================================
+    
+:run_executable
+    %EXE_DEFAULT_PATH%\%EXE%
+    exit /b %errorlevel% 
+
+:run_executable_with_debugger
+    devenv /DebugExe %EXE_DEFAULT_PATH%\%EXE%
+    exit /b 0
+
+:deepcleanup
     echo [*] Cleanup in progress ...
-    if exist external (
-        rd /s /q external
-        echo [!] external directory deleted!
+    if exist "%DEPENDENCY_DEFAULT_PATH%" (
+        rd /s /q "%DEPENDENCY_DEFAULT_PATH%"
+        echo [!] %DEPENDENCY_DEFAULT_PATH% directory deleted!
     )
+    call :cleanup
     echo [!] Cleanup done!
     exit /b 0
 
+:cleanup
+    if exist bin (
+        rd /s /q bin
+        echo [!] bin directory deleted!
+    )
+    del /s *.pdb *.obj 2>nul
+    exit /b 0
 
 :check_dependencies_are_installed
-    pushd external
-        for %%x in (%DEPENDENCIES%) do (
+    pushd %DEPENDENCY_DEFAULT_PATH%
+        for %%x in (%DEPENDENCY_LIST%) do (
             if not exist %%x (
                 echo [!] %%x directory not found!
                 call :download_dependency %%x
@@ -63,20 +159,26 @@ set CC_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Au
     exit /b 0
 
 :check_compiler_is_installed 
-
     %CC_PATH% || echo [!] Compiler %CC% not found! && goto :EOF
-
     echo [!] Compiler %CC% found!
     exit /b 0
 
 :download_dependency
     echo [*] Installing %~1 ...
     call echo [!] link: %%%~1_URL%%%
-    call curl -L --output %~1.zip %%%~1_URL%%% && echo [!] Successfully downloaded %~1!
 
-    mkdir %~1
-    tar -xf %~1.zip -C %~1 --strip-components 1
-    del %~1.zip
+    call curl -L --output %~1.zip %%%~1_URL%%% 
+    mkdir %~1 
+    tar -xf %~1.zip -C %~1 --strip-components 1 && del %~1.zip
 
+    REM (to future me) U had sdl2 setup this way in ubuntu 
+    if "%~1" == "SDL2" (
+        pushd SDL2\include
+            mkdir SDL2
+            move *.h SDL2\ >nul
+        popd
+    )
+
+    echo [!] Successfully installed %~1!
     exit /b 0
 
