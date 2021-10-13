8e3fd2353ea215ea5461ef588cbf6f9f26e53783
Gokul Krishnan C M <gokulkrishnan949@gmail.com>
2021-10-13 12:31:49 +0530

    Fixed the curl bug

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 REM Required dependencies
 set DEPENDENCIES=SDL2 GLEW
 set SDL2_URL=https://www.libsdl.org/release/SDL2-devel-2.0.16-VC.zip
-set GLEW_URL=https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-win32.zip/download
+set GLEW_URL=https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0-win32.zip
 
 
 :main
@@ -14,6 +14,15 @@ set GLEW_URL=https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-w
     
     echo [*] Running build script for windows...
     echo [*] Checking if dependenices are installed ...
+
+    if exist external (
+        echo [!] external directory found!
+    ) else (
+        echo [!] external directory not found!
+        mkdir external
+    )
+
+    echo [!] checking dependenices ...
     call :check_dependencies_are_installed
 
     exit /b 0
@@ -21,32 +30,27 @@ set GLEW_URL=https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-w
 
 
 :check_dependencies_are_installed
-    if exist external (
-        echo [!] external directory found!
-    ) else (
-        echo [!] external directory not found!
-        mkdir external
-        call :download_dependency
-    )
-    exit /b 0
-
-
-:download_dependency
     pushd external
         for %%x in (%DEPENDENCIES%) do (
             if not exist %%x (
                 echo [!] %%x directory not found!
                 echo [!] Creating %%x directory!
                 mkdir %%x
-                echo [*] Installing %%x ...
-
-                REM TODO premission denied bullshit 
-                call curl %%%%x_URL%% --output %%x
+                call :download_dependency %%x
 
             ) else (
                 echo [!] %%x folder found!
             )
         )
+    popd
+    exit /b 0
+
+
+:download_dependency
+    pushd %~1
+        echo [*] Installing %~1 ...
+        call echo [!] link: %%%~1_URL%%%
+        call curl -L -O %%%~1_URL%%% 
     popd
     exit /b 0
 
